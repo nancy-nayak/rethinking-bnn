35784d7a060ef8f5f7a565659ce2b6124ef97e6e
v-i-s-h <get_vichu@yahoo.com>
2019-11-12 15:03:52 +0530

    Added Generic CNN and LeNet5

--- src/models/basic.py
+++ src/models/basic.py
@@ -39,41 +39,54 @@ class default(HParams):
         beta_2 = 0.999
     )
 
-@registry.register_hparams(fcn)
-class l5(default):
-    hidden_units = [128, 128, 64, 64, 32]
 
-# Simple fully connected network
+# Simple Convolutional network
 @registry.register_model
-def fcn5(hparams, input_shape, num_classes):
+def cnn(hparams, input_shape, num_classes):
 
-    # 1. Create Input layer
-    input_layer = tf.keras.layers.Input(shape=input_shape)
-    # 2. Input is an image, flatten it out to use with Dense layers
-    x = tf.keras.layers.Flatten()(input_layer)
-    # 3. Create hidden layers with hidden units specified in `hparams`
+    in_layer = tf.keras.layers.Input(shape=input_shape)
+    
+    # Make convolutional layers followed by pooling
+    x = in_layer
+    for n_filters in hparams.filters:
+        x = tf.keras.layers.Conv2D(filters=n_filters, 
+                             kernel_size=hparams.kernel_size,
+                             activation=hparams.activation)(x)
+        x = tf.keras.layers.AveragePooling2D()(x)
+    # Make dense layer
+    x = tf.keras.layers.Flatten()(x)
     for units in hparams.hidden_units:
-        x = tf.keras.layers.Dense(units, activation=hparams.activation)(x)
-    # 4. Make output layer
-    output_layer = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+        x = tf.keras.layers.Dense(units=units, activation=hparams.activation)(x)
+    
+    # Make output
+    out_layer = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
 
-    # Create model and return
-    return tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
+    # Return model
+    return tf.keras.models.Model(inputs=in_layer, outputs=out_layer)
 
-@registry.register_hparams(fcn5)
+@registry.register_hparams(cnn)
 class default(HParams):
-    # Network architecture related hyper parameters
     activation = "relu"
-    hidden_units = [128, 64, 64, 32]
+    filters = [4, 4]
+    kernel_size = (3, 3)
+    hidden_units = [64, 64]
 
-    # Training related hyperparameters
-    epochs = 5
+    epochs = 10
     batch_size = 32
 
-    # Optimizer and associated hyperparameters
-    optimizer = "Adam" # Use Keras string identifier here
-    # ... followed by the parameters
-    lr = 1e-3
-    beta_1 = 0.99
-    beta_2 = 0.999
-    
+    optimizer = "Adam"
+    opt_param = dict(
+        lr = 1e-3,
+        beta_1 = 0.99,
+        beta_2 = 0.999
+    )
+
+@registry.register_hparams(cnn)
+class lenet5(default):
+    """
+        Architecture of classic LeNet-5
+        Source: https://engmrk.com/lenet-5-a-classic-cnn-architecture/
+    """
+    filters = [6, 16]
+    kernel_size = (5, 5)
+    hidden_units = [120, 84]
